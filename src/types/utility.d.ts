5cddae042489e08ffe5145e11d10a2ebec18a8af
Tauri Kalve <tauri@digiway.ee>
2022-03-10 04:00:52 +0200

    updated svg type

--- src/types/utility.d.ts
+++ src/types/utility.d.ts
@@ -2,7 +2,7 @@ declare module '*.scss';
 declare module '*.css';
 
 declare module '*.svg' {
-  import React from 'react';
+  import type React from 'react';
   const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
   export default SVG;
 }
